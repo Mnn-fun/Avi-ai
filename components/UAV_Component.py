e5e896f64b558127d6e888e55fb5c4c5bc7c55e7
Manan <manankotiya1805@gmail.com>
2025-06-25 02:53:37 +0530

    _

--- components/UAV_Component.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Use the absolute path
-df = pd.read_excel(r"c:\Users\Manan\Desktop\TRY AYTHING\Avi-ai\components\UAV_Component.xlsx")
-
-markdown = df.to_markdown(index=False)
-print(markdown)
